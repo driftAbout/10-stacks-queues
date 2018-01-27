8e1468de4e44c0aa440dc008e1e13b1be9181486
Kevin Miller <kevin@driftabout.com>
2018-01-27 12:29:48 -0800

    finished testing the queue class

--- lab-kevin/lib/queue.js
+++ lab-kevin/lib/queue.js
@@ -1,30 +1,36 @@
 const Nd = require('./nd');
 
-class Queue {
-  constructor() {
+module.exports = class {
+  
+  constructor(max_size=1048) {
     this.head = null;
     this.tail = null;
+    this.max_size = max_size;
+    this.size = 0;
   }
 
   enqueue(val) {
     //if val is undefined then return null
-    if(val === undefined) return null;
+    if(val === undefined) throw new Error('Invalid input: Value is undefined');
+    if (this.size === this.max_size) throw new Error('Queue Overflow: Queue is at Max-size');
     let nd = new Nd(val);
     nd.next = this.head;
     this.head = nd;
     //if tail is empty set nd as the tail; 
     if(!this.tail) this.tail = nd;
+    this.size++;
     return this;
   }
 
   dequeue() {
-    //if this.head is undefined, set the new node to the head
-    if(!this.head) return this;
-    //iterate the link list unto]il you find the end theb remove the node
-    for(var node = this.head; node.next; node = node.next);
+    //if this.head is undefined, return this
+    if(!this.size) return null;
+    //iterate the link list until you find the first from end
+    for(var node = this.head; node.next.next; node = node.next);
+    //remove the last node before adding updating the tail
     node.next = null;
     //set the last item to the tail
     this.tail = node;
+    this.size--;
   }
-  
-}
+};
